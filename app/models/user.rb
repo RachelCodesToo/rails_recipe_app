eff6e6ccfa272afffbe4c30edd7ae1e4b94c97b1
RachelCodesToo <a.r.fausto@gmail.com>
2020-10-26 22:30:50 -0400

    model changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :recipes
+    has_many :recipes, foreign_key: :user_id
     has_many :reviews
     has_many :reviewed_recipes, through: :reviews, source: :recipe
 
