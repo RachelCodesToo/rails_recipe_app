dfd8bd193c74a632fedb18bd19634efbe265a1d6
RachelCodesToo <a.r.fausto@gmail.com>
2020-11-06 01:35:12 -0500

    fixed new session form and sessions controller for create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,23 @@
 class SessionsController < ApplicationController
 
-    def home 
-    end
+
+    def new
+    end 
+
 
     def destroy 
         session.clear
         redirect_to root_path
     end 
 
-
+    def create
+        @user = User.find_by(username: params[:username])
+            if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to @user
+        else 
+            flash[:message] = "Invalid login info, try again."
+            render :new
+        end 
+    end 
 end 
