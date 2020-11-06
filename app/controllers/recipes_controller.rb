cf8128a2f319b98fc9c08b063960121b8c5355c4
RachelCodesToo <a.r.fausto@gmail.com>
2020-11-06 03:34:26 -0500

    updated error in application controller, set up recipes_controller and new view in recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,24 @@
 class RecipesController < ApplicationController
     before_action :redirect_if_not_logged_in
+
+    def new 
+        @recipe = Recipe.new
+    end 
+
+    def create
+        @recipe = current_user.recipes.build(recipe_params)
+        if @recipe.save
+            redirect_to recipes_path
+        else
+            render :new
+        end
+    end 
+
+    private
+
+    def recipe_params
+        params.require(:recipe).permit(:recipeName, :recipeInstructions)
+    end 
+
+
 end
