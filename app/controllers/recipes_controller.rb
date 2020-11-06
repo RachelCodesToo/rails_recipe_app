3adc3ad3ba9d0367c5d38791db165bfd6e930686
RachelCodesToo <a.r.fausto@gmail.com>
2020-11-06 12:57:40 -0500

    added edit and show to recipe's controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,20 @@ class RecipesController < ApplicationController
     before_action :redirect_if_not_logged_in
 
     def new 
+       if params[:user_id] && @user = User.find_by_id(params[:user_id])
+        @recipe = @user.recipes.build
+       else 
         @recipe = Recipe.new
+       end 
+    end 
+
+    def index 
+        if params[:user_id] && User.find_by_id(params[:user_id])
+            @recipe = @user.recipes.alpha
+        else 
+            @error = "That user doesn't exist" if params[:user_id]
+            
+        end 
     end 
 
     def create
@@ -13,6 +26,13 @@ class RecipesController < ApplicationController
             render :new
         end
     end 
+
+    def show 
+    end 
+
+
+
+
 
     private
 
