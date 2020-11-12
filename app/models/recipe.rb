b5c7d0fbcb2f93e6e6df97c7b957cff570501366
RachelCodesToo <a.r.fausto@gmail.com>
2020-11-12 12:00:49 -0500

    removed most_recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,8 +5,7 @@ class Recipe < ApplicationRecord
   validates :recipeName, :recipeInstructions, presence: true
   
   scope :alpha, -> { order(:recipeName)}
-  scope :most_reviews, -> {left_joins(:reviews).group('recipes.id').order('count(reviews.recipe_id) desc')}
-
+  
   
 
 end
