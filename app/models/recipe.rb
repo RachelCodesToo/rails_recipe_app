b6252eea33f4be248d239f406757f520f0e2afe7
RachelCodesToo <a.r.fausto@gmail.com>
2020-11-12 08:17:13 -0500

    fixed show pages to show correct content and links

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :reviews
   has_many :users, through: :reviews
+  validates :recipeName, :recipeInstructions, presence: true
+  
+
 end
