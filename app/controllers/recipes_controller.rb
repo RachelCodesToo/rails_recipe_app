8cd79c2fe88df055f476e360a0d378d9678a1426
RachelCodesToo <a.r.fausto@gmail.com>
2020-11-12 01:12:58 -0500

    fixed recipe and reviews controller, fixed index for recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,21 +1,15 @@
 class RecipesController < ApplicationController
     before_action :redirect_if_not_logged_in
-   
+    before_action :set_blog, only: [:show, :edit, :update, :destroy]
+    before_action :redirect_if_not_logged_in, only: [:update, :destroy]
+    
     def new 
        if params[:user_id] && @user = User.find_by_id(params[:user_id])
         @recipe = @user.recipes.build
        else 
         @recipe = Recipe.new
        end 
-    end 
- 
-    def create
-        @recipe = current_user.recipes.build(recipe_params)
-        if @recipe.save
-            redirect_to recipes_path(@recipe)
-        else
-            render :new
-        end
+       
     end 
     
     def index 
@@ -25,24 +19,44 @@ class RecipesController < ApplicationController
             @error = "That user doesn't exist" if params[:user_id]
             @recipe = Recipe.all
         end 
+        @posts = @posts.search(params[:q].downcase) if params[:q] && !params[:q].empty?
     end 
-
+ 
+    def create
+        @recipe = current_user.recipes.build(recipe_params)
+        if @recipe.save
+            redirect_to recipes_path
+        else
+            render :new
+        end
+    end 
     
+    def show; end 
 
-    def show 
-        @recipe = Recipe.find_by_id(params[:id])
-        redirect_to recipes_path if !@recipe
+    def destroy
+        @recipe.destroy
+        redirect_to current_user
     end 
 
+    def edit; end 
 
-
-
+    def update
+        if @recipe.update(recipe_params)
+            redirect_to recipe_path(@recipe)
+        else 
+            render :edit 
+        end
+    end
 
     private
 
     def recipe_params
         params.require(:recipe).permit(:recipeName, :recipeInstructions)
     end
+
+    def redirect_if_not_logged_in
+        redirect_to recipes_path if @recipe.user != current_user
+    end 
 
 end 
 
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -44,6 +44,9 @@ class ReviewsController < ApplicationController
     def edit 
     end 
 
+    def update
+    end 
+
     private
 
     def review_params
