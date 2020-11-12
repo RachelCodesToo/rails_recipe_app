69587cec9067c99c56eafe0c6ac650658685583f
RachelCodesToo <a.r.fausto@gmail.com>
2020-11-12 07:06:16 -0500

    fixed recipe index

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,7 +13,12 @@ class RecipesController < ApplicationController
     end 
     
     def index 
+        if params[:user_id] && @user = User.find_by_id(params[:user_id])
+            @recipe = @user.recipes
+        else 
+            @error = "That user doesn't exist" if params[:user_id]
             @recipe = Recipe.all
+        end 
     end 
  
     def create
