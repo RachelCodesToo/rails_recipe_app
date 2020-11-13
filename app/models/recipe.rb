294b40d1ae74aed0422a42e764c86aedc4e528f3
RachelCodesToo <a.r.fausto@gmail.com>
2020-11-13 10:09:01 -0500

    added some validations, added _recipe back in show

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
   validates :recipeInstructions, presence: true
-  validates :recipeName, uniqueness: true, presence: true
+  validates :recipeName, uniqueness: true, presence: true, length: { minimum: 5 }
   
   scope :alpha, -> { order(:recipeName)}
   scope :most_reviews, -> {joins(:reviews).group('recipe.id').order('count(post.id)desc')}
