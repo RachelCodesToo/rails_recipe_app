8be81421bdb1162ed4f5f476cfb27e759ed1c623
RachelCodesToo <a.r.fausto@gmail.com>
2020-11-13 10:03:32 -0500

    added a uniqueness validation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,9 +2,11 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :reviews
   has_many :users, through: :reviews
-  validates :recipeName, :recipeInstructions, presence: true
+  validates :recipeInstructions, presence: true
+  validates :recipeName, uniqueness: true, presence: true
   
   scope :alpha, -> { order(:recipeName)}
   scope :most_reviews, -> {joins(:reviews).group('recipe.id').order('count(post.id)desc')}
+
 
 end
